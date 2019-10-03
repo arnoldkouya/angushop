2158672743ffedf3280b407d22ef0a5a8a78ba43
DEV <arnoldkouya@gmail.com>
2019-10-03 22:05:55 +0200

    start theming with w3layout theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './header/top/top.component';
+import { SearchComponent } from './header/search/search.component';
+import { HeadComponent } from './header/head/head.component';
+import { BannerComponent } from './header/banner/banner.component';
+import { WelcomeComponent } from './body/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    SearchComponent,
+    HeadComponent,
+    BannerComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
